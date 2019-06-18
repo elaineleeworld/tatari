1c0597a3add4310a0e5fd8eb5bc67cd239c16a32
Elaine Lee <elaine.lee@ff0000.com>
2019-06-18 15:45:22 -0700

    Updates

---
+++ precache-manifest.202976c0b7862fb79aa7b42b15acbb8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719759b3ccd631d058e839e2e1e4b591",
+    "url": "/tatari/index.html"
+  },
+  {
+    "revision": "99768c0e600618e3d6bb",
+    "url": "/tatari/static/css/main.4a6bf85f.chunk.css"
+  },
+  {
+    "revision": "07e7f84b5bb0806304e8",
+    "url": "/tatari/static/js/2.6907acd1.chunk.js"
+  },
+  {
+    "revision": "99768c0e600618e3d6bb",
+    "url": "/tatari/static/js/main.6ce815ae.chunk.js"
+  },
+  {
+    "revision": "4e34a95dfc8d31a4d376",
+    "url": "/tatari/static/js/runtime~main.09c043c1.js"
+  },
+  {
+    "revision": "596996bf73ae7e556d01bedeb8481ce1",
+    "url": "/tatari/static/media/rotations.596996bf.csv"
+  },
+  {
+    "revision": "8fcdaafb627df958c27f4a3516b0f7ab",
+    "url": "/tatari/static/media/spots.8fcdaafb.csv"
+  }
+]);
